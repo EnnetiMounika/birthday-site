9e0a8adffc56176ed4bb3b29b186fabc35c80ae6
EnnetiMounika <53477699+EnnetiMounika@users.noreply.github.com>
2021-04-22 11:55:01 +0530

    My first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Pandi" && b=="23-04-1997")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
